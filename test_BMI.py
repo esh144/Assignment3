25e33c45ced5a8809bec4767aa21deb289cf28c9
Marcus Brumfield <brumfieldmarcus@gmail.com>
2016-06-20 11:56:51 -0500

    Create test_BMI.py

---
+++ test_BMI.py
@@ -0,0 +1,9 @@
+# Test BMI function
+
+def test_BMI():
+    height_feet = 5
+    height_inches = 3
+    weight_lbs = 125
+
+    assert BMI(height_feet, height_inches, weight_lbs) == "Normal Weight"
+    
