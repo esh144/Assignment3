1eca57b3a29729b53e5142b6fe07039cfa73eeae
Marcus Brumfield <brumfieldmarcus@gmail.com>
2016-06-20 22:48:56 -0500

    Update test_BMI.py

--- test_BMI.py
+++ test_BMI.py
@@ -1,9 +1,11 @@
 # Test BMI function
 
+from bmi import *
+
 def test_BMI():
     height_feet = 5
     height_inches = 3
     weight_lbs = 125
 
-    assert BMI(height_feet, height_inches, weight_lbs) == "Normal Weight"
-    
+    assert BMI(height_feet, height_inches, weight_lbs) == "Normalweight"
+
