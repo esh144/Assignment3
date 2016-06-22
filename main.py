3da9f0344d9ddd71227211aee3e81ec35e517734
Marcus Brumfield <brumfieldmarcus@gmail.com>
2016-06-22 09:39:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from retire import *
 
 #Include Distance formula file
 
+
+loop = True
+
 print("Function Menu")
 print("-------------")
 print("1.) Body Mass Index")
@@ -15,23 +18,37 @@ print("2.) Retirement")
 print("3.) Distance Formula")
 print("0.) Quit")
 print('')
-function_choice = int(input("Function choice: "))
-while function_choice != 0:
-    if function_choice == 1:
-        feet = int(input("Enter height as a integer: "))
-        inches = int(input("Enter remaining height in inches: "))
-        weight = int(input("Enter weight in pounds: "))
-        result = BMI(feet, inches, weight)
-        print("BMI Result: You are ", result)
-    
-    elif function_choice == 2:
-        age = int(input("Enter your current age: "))
-        annual_sal = int(input("Enter your annual salary: "))
-        percentage = int(input("Enter the percent you wish to save from your annual salary (integer only):"))
-        goal = int(input("What is your desired goal for your retirement savings?"))
-        result = calucate_years_to_goal (age, annual_sal, percentage, goal)
-        print(result)
+
+while loop == True:
+    try:
+        function_choice = int(input("Enter valid Function choice: "))
+    except:
+        print("Enter valid choice")
+    else:
+        if function_choice == 1:
+            print("")
+            print("Body Mass Index")
+            print("---------------")
+            feet = int(input("Enter height as a integer: "))
+            inches = int(input("Enter remaining height in inches: "))
+            weight = int(input("Enter weight in pounds: "))
+            result = BMI(feet, inches, weight)
+            print("BMI Result: You are ", result)
+            print("")
         
-    print("")
-    function_choice = int(input("Enter another function choice: "))
+        elif function_choice == 2:
+            age = int(input("Enter your current age: "))
+            annual_sal = int(input("Enter your annual salary: "))
+            percentage = int(input("Enter the percent you wish to save from your annual salary (integer only):"))
+            goal = int(input("What is your desired goal for your retirement savings?"))
+            result = calucate_years_to_goal (age, annual_sal, percentage, goal)
+            print(result)
+
+        elif function_choice == 0:
+            print("Program ended.")
+            break
+
+            
+            
+
 
