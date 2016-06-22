dc592827576a9a20e851eae966c9fbf29b17eb6f
esh144 <esh144@msstate.edu>
2016-06-21 21:36:27 -0500

    Update and rename test_distance to test_distance.py

--- test_distance.py
+++ test_distance.py
@@ -1,15 +1,23 @@
 #Test the Distance Formula application
+from distanceformula import *
 
-def Test1():
+def test_1():
     x1 = 5
     y1 = 3
     x2 = 10
     y2 = 15
     assert DistanceFormula(x1, y1, x2, y2) == 13
 
-def Test2():
+def test_2():
     x1 = 10
     y1 = 15
     x2 = 5
     y2 = 3
     assert DistanceFormula(x1, y1, x2, y2) == 13
+
+def test_3():
+    x1 = "!"
+    y1 = 9
+    x2 = 4
+    y2 = 15
+    assert DistanceFormula(x1, y1, x2, y2) == "Invalid Input"
