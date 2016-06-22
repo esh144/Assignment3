27c7fbdecc9a0514f8a097ed982e503036656d0b
jgn46 <jgn46@msstate.edu>
2016-06-21 23:47:05 -0500

    Update test_retirement.py

--- test_retirement.py
+++ test_retirement.py
@@ -1,15 +1,15 @@
-def test_savings_goal1():
-	current_age = 46
+def test_savings_goal1(x):
+	age = 46
 	annual_sal = 98000
-	percent_saved = (.10 * annual_sal) * 2
+	percentage = 10
 	goal = 1000000
 
 	assert(current_age, annual_sal, percent_saved, goal) = 51
 	
-def test_savings_goal2():
-	current_age = 46
+def test_savings_goal2(x):
+	age = 46
 	annual_sal = 98000
-	percent_saved = (.10 * annual_sal) * 2
+	percentage = 10
 	goal = 1000000
 
 	assert(current_age, annual_sal, percent_saved, goal) = 44
