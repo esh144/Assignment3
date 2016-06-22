21a9cfc1e448bbaccdb96dcef6201bfcd7ab3787
Marcus Brumfield <brumfieldmarcus@gmail.com>
2016-06-22 10:15:30 -0500

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,18 +1,31 @@
 # Body Mass Index Function 
 
 def BMI(height, inches, weight):
-    weight_kg = weight * 0.45
-    height_in = height * 12
-    height_m = (height_in + inches) * 0.025
-    answer = height_m * height_m
-    bmi = weight_kg / answer
-    print(bmi)
+    try:
+        height = int(height)
+        inches = int(inches)
+        weight = int (weight)
+        
+        
+    except:
+        result = "invalid"
+    
+    else: 
+        weight_kg = weight * 0.45
+        height_in = height * 12
+        height_m = (height_in + inches) * 0.025
+                  
+        answer = height_m * height_m
+        bmi = weight_kg / answer
+        print("BMI: ", bmi)
 
-    if bmi <= 18.5:
-        return "Underweight"
-    if bmi > 18.5 and bmi <= 24.9:
-        return "Normalweight"
-    if bmi >= 25 and bmi <= 29.9:
-        return "Overweight"
-    else:
-        return "Obesity"
+        if bmi <= 18.5:
+            result = "Underweight"
+        if bmi > 18.5 and bmi <= 24.9:
+            result = "Normalweight"
+        if bmi >= 25 and bmi <= 29.9:
+            result = "Overweight"
+        else:
+            result = "Obesity"
+
+    return result
